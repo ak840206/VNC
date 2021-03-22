e345e8e9eecd0cfb1558cfe727d98b2d09efa7ba
Christopher Rhodes <chrhodes@cox.net>
2021-03-21 17:35:12 -0700

    Name change

--- VNC.CodeAnalysis/VNC.CodeAnalysis/Common.cs
+++ VNC.CodeAnalysis/VNC.CodeAnalysis/Common.cs
@@ -2,7 +2,7 @@
 {
     public class Common
     {
-        public const string PROJECT_NAME = "VNC.CodeAnalysis";
+        public const string APPLICATION_NAME = "VNC.CodeAnalysis";
         public const string LOG_CATEGORY = "VNCCodeAnalysis";
         //public const string APPNAME = "Code Command Console";
 
