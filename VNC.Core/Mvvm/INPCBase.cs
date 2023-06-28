f549bad6a1443a495cfc98e2f41d603a089be2d2
Christopher Rhodes <chrhodes@cox.net>
2023-06-28 10:30:51 -0700

    Added INotifyPropertyChanged base class

---
+++ VNC.Core/Mvvm/INPCBase.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VNC.Core.Mvvm
+{
+    public class INPCBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This is the traditional approach - requires string name to be passed in
+
+        //private void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+
+        // This is the new CompilerServices attribute!
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            // TODO(crhodes)
+//            // Decide if we really want to log this.
+//#if LOGGING
+//            long startTicks = Log.VIEWMODEL_LOW($"Enter ({propertyName})", Common.LOG_CATEGORY);
+//#endif
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+//#if LOGGING
+//            Log.VIEWMODEL_LOW("Exit", Common.LOG_CATEGORY, startTicks);
+//#endif
+        }
+
+        #endregion
+    }
+}
